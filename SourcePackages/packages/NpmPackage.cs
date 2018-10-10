447254f206dd011dc695eca027cce2e6c37eb1dd
Tobias <tobias@raqt.com>
2018-10-10 16:12:08 +0200

    Added ignored files

---
+++ SourcePackages/packages/NpmPackage.cs
@@ -0,0 +1,136 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SourcePackages.packages
+{
+    public class NpmPackage
+    {
+        public static string NpmRegistry { get; set; }
+        public static readonly string NpmPath;
+        static NpmPackage()
+        {
+            NpmPath = FindNpmPath("npm.cmd");
+
+        }
+
+        public static async Task LoopPackageJson(string packageJsonPath = @"C:\git\RayCare\src\adapters\RayCare.Web.UI\package.json")
+        {
+            string[] packageJson = Directory.GetFiles(@"c:\git\RayCare", "package.json", SearchOption.AllDirectories);
+            await Task.Run(() =>
+            {
+                foreach (var item in packageJson)
+                {
+                    if (!item.Contains("node_modules"))
+                    {
+                        ReadPackageJsonFile(item);
+                    }
+                    else
+                    {
+                        //Skips this for the moment - here we can get all in node_nodules also
+                        //ReadPackageJsonFile(item);
+                    }
+
+                }
+            });
+        }
+
+        private static void ReadPackageJsonFile(string item)
+        {
+            dynamic o1 = JObject.Parse(File.ReadAllText(item));
+            IList<JToken> jsonDevDep = o1["devDependencies"];
+            if (jsonDevDep != null)
+            {
+                foreach (var jToken in jsonDevDep)
+                {
+                    var p = (JProperty)jToken;
+                    //var license = await RunNpmViewCheckLicense(p.Name, p.Value.ToString());
+                    //await Utils.AddToPackageInformation(new PackageInformation { PackageName = p.Name, PackageVersion = p.Value.ToString(), PackageDescription = "", OriginOfPackage = PackageInformation.Origin.Npm, FeedRegistry = NpmRegistry });
+                    FactoryPackages.AddPackage(new NugetPackage { Name = p.Name, Version = p.Value.ToString(), UniqueName = "devDependencies", PackageType = PackageType.Npm });
+                }
+            }
+            IList<JToken> jsonDep = o1["dependencies"];
+            if (jsonDep != null)
+            {
+                foreach (var jToken in jsonDep)
+                {
+                    var p = (JProperty)jToken;
+                    //var license = await RunNpmViewCheckLicense(p.Name, p.Value.ToString());
+                    //await Utils.AddToPackageInformation(new PackageInformation { PackageName = p.Name, PackageVersion = p.Value.ToString(), PackageDescription = "", OriginOfPackage = PackageInformation.Origin.Npm, FeedRegistry = NpmRegistry });
+                    FactoryPackages.AddPackage(new NugetPackage { Name = p.Name, Version = p.Value.ToString(), UniqueName = "dependencies", PackageType = PackageType.Npm });
+                }
+
+
+            }
+        }
+
+        public static string FindNpmPath(string npmCmd)
+        {
+            npmCmd = Environment.ExpandEnvironmentVariables(npmCmd);
+            if (!File.Exists(npmCmd))
+            {
+                if (Path.GetDirectoryName(npmCmd) == string.Empty)
+                    foreach (var test in (Environment.GetEnvironmentVariable("PATH") ?? "").Split(';'))
+                    {
+                        var path = test.Trim();
+                        if (!string.IsNullOrEmpty(path) && File.Exists(path = Path.Combine(path, npmCmd)))
+                            return Path.GetFullPath(path);
+                    }
+
+                throw new FileNotFoundException(new FileNotFoundException().Message, npmCmd);
+            }
+
+            return Path.GetFullPath(npmCmd);
+        }
+
+        public static async Task<string> CheckNpmRegistry()
+        {
+            string output = null;
+            try
+            {
+                //return output;
+                var psiNpmRunDist = new ProcessStartInfo
+                {
+                    FileName = NpmPath,
+                    Arguments = $"config get registry",
+                    RedirectStandardInput = true,
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    StandardOutputEncoding = Encoding.UTF8
+                };
+                await Task.Run(() =>
+                {
+                    var pNpmRunDist = Process.Start(psiNpmRunDist);
+
+
+                    if (pNpmRunDist != null)
+                    {
+                        try
+                        {
+                            output = pNpmRunDist.StandardOutput.ReadToEnd();
+                        }
+                        catch (Exception)
+                        {
+                            output = null;
+                        }
+
+                        pNpmRunDist.StandardInput.WriteLine("npm config get registry & exit");
+                        pNpmRunDist.WaitForExit();
+                    }
+                });
+            }
+            catch (Exception e)
+            {
+                output = null;
+
+            }
+            NpmRegistry = output;
+            return output;
+        }
+    }
+}
---
+++ SourcePackages/packages/NugetPackage.cs
@@ -0,0 +1,127 @@
+﻿using NuGet;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace SourcePackages.packages
+{
+    public class NugetPackage : IPackageMagic
+    {
+        public string Name { get; set; }
+        public string Version { get; set; }
+        public PackageType PackageType { get; set; }
+        public string UniqueName { get; set; }
+
+        private static async Task SearchNugetPackageReferences(string file)
+        {
+            await Task.Run(() =>
+            {
+                XmlDocument xDoc = new XmlDocument();
+
+                using (var fs = new FileStream(file, FileMode.Open, FileAccess.Read))
+                {
+                    xDoc.Load(fs);
+
+                    // Load Xml
+
+                    var nodes = xDoc.GetElementsByTagName("PackageReference");
+
+                    foreach (XmlNode node in nodes)
+                    {
+                        if (node.Attributes != null)
+                        {
+                            string packageVersion;
+                            if (node.Attributes["Version"] == null)
+                            {
+                                packageVersion = node.Attributes["version"].Value;
+                            }
+                            else
+                            {
+                                packageVersion = node.Attributes["Version"].Value;
+                            }
+
+                            //await Utils.AddToPackageInformation(new PackageInformation { PackageName = node.Attributes["Include"].Value, PackageVersion = packageVersion, PackageDescription = "", OriginOfPackage = PackageInformation.Origin.PackageReference });
+                            FactoryPackages.AddPackage(new NugetPackage { Name = node.Attributes["Include"].Value, Version = packageVersion, UniqueName = "PackageReference", PackageType = PackageType.Nuget });
+                        }
+
+                    }
+
+
+
+                }
+            });
+
+        }
+        public static async Task SearchForAllPackageReferences(string projectDirectory)
+        {
+
+            string[] csProjFiles = Directory.GetFiles(projectDirectory, "*.csproj", SearchOption.AllDirectories);
+            foreach (var csProjFile in csProjFiles)
+            {
+                await SearchNugetPackageReferences(csProjFile);
+            }
+
+        }
+        public static async Task SearchForPackagesConfig(string projectDirectory)
+        {
+            await Task.Run(() =>
+            {
+                string[] packagesConfig = Directory.GetFiles(projectDirectory, "packages.config", SearchOption.AllDirectories);
+
+                foreach (var packConfig in packagesConfig)
+                {
+                    //NugetPackages p = new NugetPackages();
+                    var directoryName = Path.GetDirectoryName(packConfig);
+                    string foundCsProjFile = "";
+                    if (directoryName != null)
+                    {
+                        string[] proj = Directory.GetFiles(directoryName, "*.csproj", SearchOption.TopDirectoryOnly);
+
+                        if (proj.Length > 0)
+                        {
+                            foundCsProjFile = proj[0];
+                        }
+                        else
+                        {
+                            foundCsProjFile = "No csProj file found " + packConfig;
+                        }
+                    }
+
+
+                    var file = new PackageReferenceFile(packConfig);
+                    foreach (PackageReference packageReference in file.GetPackageReferences())
+                    {
+                        FactoryPackages.AddPackage(new NugetPackage { Name = packageReference.Id, Version = packageReference.Version.ToNormalizedString(), UniqueName = "PackageConfig", PackageType = PackageType.Nuget });
+                        //Utils.AddToPackageInformation(new PackageInformation { PackageName = packageReference.Id, PackageVersion = packageReference.Version.ToNormalizedString(), PackageDescription = foundCsProjFile, OriginOfPackage = PackageInformation.Origin.PackageConfig, CsProjFile = foundCsProjFile }).GetAwaiter().GetResult();
+
+                    }
+                }
+            });
+        }
+    }
+
+
+
+    //[XmlRoot(ElementName = "package")]
+    //public class Package
+    //{
+    //    [XmlAttribute(AttributeName = "id")]
+    //    public string Id { get; set; }
+    //    [XmlAttribute(AttributeName = "targetFramework")]
+    //    public string TargetFramework { get; set; }
+    //    [XmlAttribute(AttributeName = "version")]
+    //    public string Version { get; set; }
+    //}
+
+    //[XmlRoot(ElementName = "packages")]
+    //public class Packages
+    //{
+    //    [XmlElement(ElementName = "package")]
+    //    public List<Package> Package { get; set; }
+    //}
+}
