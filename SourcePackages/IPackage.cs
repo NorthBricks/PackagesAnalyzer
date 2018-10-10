143b174f00d390926a25e819a2d71bfee455952d
Tobias <tobias@raqt.com>
2018-10-10 16:11:21 +0200

    Add project files.

---
+++ SourcePackages/IPackage.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace SourcePackages
+{
+    public enum PackageType
+
+    {
+
+        Nuget,
+
+        Npm
+
+    }
+    public interface IPackageMagic
+    {
+        string Name { get; set; }
+        string Version { get; set; }
+
+        string UniqueName { get; set; }
+        PackageType PackageType { get; set; }
+    }
+
+    public static class FactoryPackages
+    {
+        static List<IPackageMagic> packages;
+        static FactoryPackages()
+        {
+            packages = new List<IPackageMagic>();
+        }
+
+        public static List<IPackageMagic> GetPackages()
+        {
+            return packages;
+        }
+
+        public static void AddPackage(IPackageMagic package)
+        {
+            IPackageMagic checkIfExist = packages.Find(z => z.Name == package.Name && z.Version == package.Version);
+            if (checkIfExist == null)
+            {
+                packages.Add(package);
+
+            }
+            else
+            {
+                //Console.WriteLine("Skipped - " + package.Name + " " + package.Version);
+            }
+
+        }
+        public static void RemovePackage(IPackageMagic package)
+        {
+            packages.Remove(package);
+        }
+
+    }
+
+
+
+    //public class Npm : IPackage
+    //{
+    //    public string Name { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+    //}
+}
---
+++ SourcePackages/Program.cs
@@ -0,0 +1,34 @@
+﻿using NuGet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SourcePackages.packages;
+namespace SourcePackages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RunAsync().GetAwaiter().GetResult();
+            Console.WriteLine(FactoryPackages.GetPackages().Count);
+            foreach (var item in FactoryPackages.GetPackages().OrderBy(x => x.Name))
+            {
+
+                Console.WriteLine(item.PackageType + "|" + item.Name + " " + item.Version + "|" + item.UniqueName);
+            }
+        }
+
+        static async Task RunAsync()
+        {
+            List<Task> myTasks = new List<Task>
+            {
+            NugetPackage.SearchForPackagesConfig(@"C:\git\RayCare"),
+            NugetPackage.SearchForAllPackageReferences(@"C:\git\RayCare"),
+            NpmPackage.LoopPackageJson()
+            };
+            await Task.WhenAll(myTasks);
+        }
+    }
+}
